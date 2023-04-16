8eae8c7221a8d6fa728529219b713f58ed73f622
mahiiram <mahendran.ramar9797@gmail.com>
2023-04-16 20:45:00 +0530

    firstcommit

---
+++ src/components/data.js
@@ -0,0 +1,11 @@
+const Array = [
+    { id:"1",
+      name:"Sachin",
+      age:49,
+      email:"Sachin@gmail.com"},
+    { id:"2",
+      name:"MS Dhoni",
+      age:41,
+      email:"Dhoni@gmail.com"},
+]
+export default Array;
